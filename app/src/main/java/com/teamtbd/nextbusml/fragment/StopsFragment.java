5329daae8893889652073301ff97c7f37ab044eb
crejaud <crejaud@gmail.com>
2016-12-05 21:20:34 -0500

    clean up

--- app/src/main/java/com/teamtbd/nextbusml/fragment/StopsFragment.java
+++ app/src/main/java/com/teamtbd/nextbusml/fragment/StopsFragment.java
@@ -10,14 +10,6 @@ import android.view.ViewGroup;
 
 import com.teamtbd.nextbusml.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link StopsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link StopsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StopsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
