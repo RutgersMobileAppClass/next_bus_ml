ed8e0d4787bd70680934975e467082db823d67f3
Corentin Rejaud <crejaud@nwk--205-134.nwk.ruw.rutgers.edu>
2016-12-05 19:35:00 -0500

    add project initially

---
+++ app/src/main/java/com/teamtbd/nextbusml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teamtbd.nextbusml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
