5d46f35e37dc1389e86a36d40e920ae90cea003b
crejaud <crejaud@gmail.com>
2016-12-07 13:59:52 -0500

    alarm should schedule accordingly

--- app/src/main/java/com/teamtbd/nextbusml/MainActivity.java
+++ app/src/main/java/com/teamtbd/nextbusml/MainActivity.java
@@ -17,19 +17,21 @@ import android.view.MenuItem;
 
 import com.teamtbd.nextbusml.fragment.CoursesFragment;
 import com.teamtbd.nextbusml.fragment.StopsFragment;
+import com.teamtbd.nextbusml.model.Course;
 import com.teamtbd.nextbusml.notification.ReminderNotification;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CoursesFragment.OnCourseInteractionListener {
 
-    private final String COURSES_FILE = "COURSES_LIST.ser";
+    public static final String COURSES_FILE = "COURSES_LIST.ser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,27 +84,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void scheduleNotification(Notification notification, Date date) {
-
-        Calendar cur_cal = new GregorianCalendar();
-        cur_cal.setTimeInMillis(System.currentTimeMillis());//set the current time and date for this calendar
-
-        Calendar cal = new GregorianCalendar();
-        cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
-        cal.set(Calendar.HOUR_OF_DAY, 18);
-        cal.set(Calendar.MINUTE, 32);
-        cal.set(Calendar.SECOND, cur_cal.get(Calendar.SECOND));
-        cal.set(Calendar.MILLISECOND, cur_cal.get(Calendar.MILLISECOND));
-        cal.set(Calendar.DATE, cur_cal.get(Calendar.DATE));
-        cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
-        Intent intent = new Intent(this, ReminderNotification.class);
-        intent.putExtra(ReminderNotification.NOTIFICATION_ID, 1);
-        intent.putExtra(ReminderNotification.NOTIFICATION, notification);
-        PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
-        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30*1000, pintent);
-    }
-
     private Notification createNotification(String content) {
         Notification.Builder builder = new Notification.Builder(this);
         builder.setContentTitle("Your class is almost starting.");
@@ -110,12 +91,72 @@ public class MainActivity extends AppCompatActivity {
         return builder.build();
     }
 
-    private void refreshNotifications() {
+    @Override
+    public void addAlarm(Course course) {
+        Calendar cur_cal = new GregorianCalendar();
+        cur_cal.setTimeInMillis(System.currentTimeMillis());//set the current time and date for this calendar
+
+        int day;
+        if (course.getDay() == 0) {
+            day = Calendar.MONDAY;
+        } else if (course.getDay() == 1) {
+            day = Calendar.TUESDAY;
+        } else if (course.getDay() == 2) {
+            day = Calendar.WEDNESDAY;
+        } else if (course.getDay() == 3) {
+            day = Calendar.THURSDAY;
+        } else if (course.getDay() == 4) {
+            day = Calendar.FRIDAY;
+        } else {
+            day = Calendar.MONDAY;
+        }
+
+        // for the next 12 weeks~
+        for (int i = 0; i < 12; i++) {
+            Calendar cal = new GregorianCalendar();
+            cal.add(Calendar.DAY_OF_YEAR, cur_cal.get(Calendar.DAY_OF_YEAR));
+            cal.set(Calendar.HOUR_OF_DAY, course.getStartTime().getHour());
+            cal.set(Calendar.MINUTE, course.getStartTime().getMinute());
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            cal.set(Calendar.DAY_OF_WEEK, day);
+            cal.set(Calendar.MONTH, cur_cal.get(Calendar.MONTH));
+            cal.set(Calendar.WEEK_OF_MONTH, cur_cal.get(Calendar.WEEK_OF_MONTH));
+            cal.add(Calendar.DAY_OF_MONTH, i*7);
+            cal.add(Calendar.MINUTE, -40);
+            Intent intent = new Intent(this, ReminderNotification.class);
+            intent.putExtra(ReminderNotification.NOTIFICATION_ID, 1);
+            intent.putExtra(ReminderNotification.NOTIFICATION, createNotification(course.getTitle() + " is starting soon!"));
+            PendingIntent pintent = PendingIntent.getService(this, 0, intent, 0);
+            AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            alarm.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 30 * 1000, pintent);
+        }
+    }
+
+    @Override
+    public void refreshAlarms() {
         // delete all notifications
         NotificationManager notifManager= (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
         notifManager.cancelAll();
 
         // reschedule all notifications based off courses
+        ArrayList<Course> courses = getCoursesFromFile();
+        for (Course course: courses) {
+            addAlarm(course);
+        }
+    }
 
+    private ArrayList<Course> getCoursesFromFile() {
+        ArrayList<Course> courses = new ArrayList<>();
+        try {
+            FileInputStream fileIn = new FileInputStream(MainActivity.COURSES_FILE);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            courses = (ArrayList<Course>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (ClassNotFoundException|IOException e){
+            e.printStackTrace();
+        }
+        return courses;
     }
 }
